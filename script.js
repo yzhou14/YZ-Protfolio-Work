20d5efcd4ccfc65a2a3d73b4c968d5f487a8d717
yzhou14 <yzhou14@risd.edu>
2022-02-07 15:21:01 -0500

    work collections

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $(".sectioncover1").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+    $(".sectioncover2").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+    $(".sectioncover3").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+    $(".sectioncover4").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+    $(".sectioncover5").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+    $(".sectioncover6").hover(function(){
+        $(this).slideToggle(1000);
+    });
+
+});
