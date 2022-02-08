3668b9e3b16fd5d395d0cf615e7b08f7997c7fd2
yzhou14 <yzhou14@risd.edu>
2022-02-08 15:41:20 -0500

    update

--- script.js
+++ script.js
@@ -1,27 +1,91 @@
 $(document).ready(function(){
 
-    $(".sectioncover1").mouseover(function(){
-        $(this).slideToggle(1000);
+    $(".section1").mouseover(function(){
+        $(".sectioncover1").animate({
+            height: "0px",
+        })
     })
 
-    $(".sectioncover2").mouseover(function(){
-        $(this).slideToggle(1000);
-    });
+    $(".section1").mouseleave(function(){
+        $(".sectioncover1").animate({
+            height: "300px",
+        })
+    })
 
-    $(".sectioncover3").hover(function(){
-        $(this).slideToggle(1000);
-    });
+    // $(".sectioncover1").mouseleave(function(){
+    //     $(this).slideToggle(1000);
+    // })
 
-    $(".sectioncover4").hover(function(){
-        $(this).slideToggle(1000);
-    });
+    $(".section2").mouseover(function(){
+        $(".sectioncover2").animate({
+            height: "0px",
+        })
+    })
 
-    $(".sectioncover5").hover(function(){
-        $(this).slideToggle(1000);
-    });
+    $(".section2").mouseleave(function(){
+        $(".sectioncover2").animate({
+            height: "300px",
+        })
+    })
 
-    $(".sectioncover6").hover(function(){
-        $(this).slideToggle(1000);
-    });
+
+
+
+    $(".section3").mouseover(function(){
+        $(".sectioncover3").animate({
+            height: "0px",
+        })
+    })
+
+    $(".section3").mouseleave(function(){
+        $(".sectioncover3").animate({
+            height: "300px",
+        })
+    })
+
+
+
+
+
+    $(".section4").mouseover(function(){
+        $(".sectioncover4").animate({
+            height: "0px",
+        })
+    })
+
+    $(".section4").mouseleave(function(){
+        $(".sectioncover4").animate({
+            height: "300px",
+        })
+    })
+
+
+
+    $(".section5").mouseover(function(){
+        $(".sectioncover5").animate({
+            height: "0px",
+        })
+    })
+
+    $(".section5").mouseleave(function(){
+        $(".sectioncover5").animate({
+            height: "300px",
+        })
+    })
+
+
+
+
+    $(".section6").mouseover(function(){
+        $(".sectioncover6").animate({
+            height: "0px",
+        })
+    })
+
+    $(".section6").mouseleave(function(){
+        $(".sectioncover6").animate({
+            height: "300px",
+        })
+    })
 
 });
